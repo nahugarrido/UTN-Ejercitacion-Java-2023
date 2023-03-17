30f592c6b2f1c5f5226b10e9d063f9f97d171c1b
Nahuel Garrido <garridonahuel8@gmail.com>
2023-03-16 21:38:23 -0300

    merge

--- Guia 0 - Introduccion a JAVA/src/Main.java
+++ Guia 0 - Introduccion a JAVA/src/Main.java
@@ -1,8 +1,10 @@
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        punto4();
+        punto7();
 
     }
 
@@ -26,7 +28,7 @@ public class Main {
 
         for (int i = 0; i < 5; i++) {
             System.out.println("Ingrese un numero: ");
-             aux = scan.nextInt();
+            aux = scan.nextInt();
             arr[i] = aux;
             System.out.println("\n");
         }
@@ -34,27 +36,28 @@ public class Main {
         scan.close();
 
 
-        for(int i = 0; i < 5; i++) {
-            System.out.println(" "+ arr[i] + " ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(" " + arr[i] + " ");
         }
     }
+
     public static void punto3() {
         int i = 0;
         int number = 2;
 
         System.out.println("punto 3: \n");
 
-        while(i < 100) {
+        while (i < 100) {
             boolean isPrimo = true;
 
-            for(int j = 2; j < number; j++ ) {
-                if(number % j == 0) {
+            for (int j = 2; j < number; j++) {
+                if (number % j == 0) {
                     isPrimo = false;
                     break;
                 }
             }
 
-            if(isPrimo) {
+            if (isPrimo) {
                 i++;
                 System.out.println("EL numero : " + number + " es primo. \n");
             }
@@ -69,22 +72,22 @@ public class Main {
         System.out.println("punto 4: \n");
         System.out.println("Ingrese la cantidad de primos que quiere buscar: ");
         Scanner scan = new Scanner(System.in);
-        int limit =  scan.nextInt();
+        int limit = scan.nextInt();
         scan.close();
         System.out.println("\n");
 
 
-        while(i < limit) {
+        while (i < limit) {
             boolean isPrimo = true;
 
-            for(int j = 2; j < number; j++ ) {
-                if(number % j == 0) {
+            for (int j = 2; j < number; j++) {
+                if (number % j == 0) {
                     isPrimo = false;
                     break;
                 }
             }
 
-            if(isPrimo) {
+            if (isPrimo) {
                 i++;
                 System.out.println("EL numero : " + number + " es primo. \n");
             }
@@ -99,22 +102,22 @@ public class Main {
         System.out.println("punto 6: \n");
         System.out.println("Ingrese un numero");
         Scanner scan = new Scanner(System.in);
-        int limit =  scan.nextInt();
+        int limit = scan.nextInt();
         scan.close();
         System.out.println("\n");
 
 
-        while(i < limit) {
+        while (i < limit) {
             boolean isPrimo = true;
 
-            for(int j = 2; j < number; j++ ) {
-                if(number % j == 0) {
+            for (int j = 2; j < number; j++) {
+                if (number % j == 0) {
                     isPrimo = false;
                     break;
                 }
             }
 
-            if(isPrimo) {
+            if (isPrimo) {
                 i++;
                 System.out.println("EL numero : " + number + " es primo. \n");
             }
@@ -122,5 +125,43 @@ public class Main {
         }
     }
 
+    public static void punto7() {
 
+        System.out.println("punto 7: \n");
+
+        int start = 1;
+        int acum = 0;
+
+        for(int i = 0; i < 10; i++) {
+            acum += start;
+            start++;
+        }
+
+        System.out.println("La suma es " + acum);
     }
+
+    public static void punto8() {
+        System.out.println("punto 8: \n");
+
+        boolean flag = true;
+        int aux = 0;
+        int cont = 0;
+        char answer = 's';
+
+        while(flag) {
+            System.out.println("Ingrese un numero: ");
+                Scanner scan = new Scanner(System.in);
+                aux = scan.nextInt();
+                if( aux > 0) {
+                    cont++;
+                }
+
+                System.out.println("\nDeseas seguir ingresando valores? (s/n)");
+                
+
+
+        }
+    }
+
+
+}
