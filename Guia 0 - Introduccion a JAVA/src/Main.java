9fb67ff92981633003013b403e7b6bb617c62129
Nahuel Garrido <garridonahuel8@gmail.com>
2023-03-17 00:16:46 -0300

    terminada guia introduccion

--- Guia 0 - Introduccion a JAVA/src/Main.java
+++ Guia 0 - Introduccion a JAVA/src/Main.java
@@ -4,12 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        punto7();
-
-=======
-        punto15(46);
->>>>>>> c48ae2baaf068c75fe3e8501cf71961d9f50609d
+        punto22();
     }
 
     public static void punto1a() {
@@ -130,7 +125,6 @@ public class Main {
 
     }
 
-<<<<<<< HEAD
     public static void punto7() {
 
         System.out.println("punto 7: \n");
@@ -138,7 +132,7 @@ public class Main {
         int start = 1;
         int acum = 0;
 
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             acum += start;
             start++;
         }
@@ -146,30 +140,6 @@ public class Main {
         System.out.println("La suma es " + acum);
     }
 
-    public static void punto8() {
-        System.out.println("punto 8: \n");
-
-        boolean flag = true;
-        int aux = 0;
-        int cont = 0;
-        char answer = 's';
-
-        while(flag) {
-            System.out.println("Ingrese un numero: ");
-                Scanner scan = new Scanner(System.in);
-                aux = scan.nextInt();
-                if( aux > 0) {
-                    cont++;
-                }
-
-                System.out.println("\nDeseas seguir ingresando valores? (s/n)");
-                
-
-
-        }
-    }
-
-=======
     public static void punto8() {
         Scanner scan = new Scanner(System.in);
         boolean flag = true;
@@ -242,18 +212,139 @@ public class Main {
     }
 
     public static void punto14(int number) {
-        System.out.println("El numero elegido es: " + number + ", su doble es: " + (number*2) + " y su triple es: " + (number*3));
+        System.out.println("El numero elegido es: " + number + ", su doble es: " + (number * 2) + " y su triple es: " + (number * 3));
     }
 
     public static void punto15(int degrees) {
         //  F = 32 + ( 9 * C / 5)
-        float centigrades = ((float)degrees - 32) * 5/9;
+        float centigrades = ((float) degrees - 32) * 5 / 9;
         System.out.println(degrees + " grados Farenheit equivalen a " + centigrades + "grados Celcius");
     }
 
     public static void punto16(float radio) {
+        float longitud = (float) (2 * 3.14 * radio);
+        float area = (float) 3.14 * (radio * radio);
+        System.out.println("La logintud de la circunferencia es: " + longitud);
+        System.out.println("El area de la circunferencia es: " + area);
+    }
+
+    public static void punto17(int km) {
+        float miles = (float) 1.609;
+        System.out.println("Una velocidad de " + km + " km/h equivale a una velocidad de " + ((km / miles) / 3600) + " m/s ");
+    }
+
+    public static double punto18(double c1, double c2) {
+        double result = Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
+        System.out.println("El valor de la hipotenusa es: " + result);
+        return result;
+    }
+
+    public static void punto19() {
+        double[] arr = new double[30];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.random() * 30;
+        }
+
+        double tempMin = arr[0];
+        double tempMax = arr[0];
+        double acum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            tempMin = Math.min(tempMin, arr[i]);
+            tempMax = Math.max(tempMax, arr[i]);
+            acum += arr[i];
+        }
+
+        System.out.println("La temperatura minima es: " + tempMin);
+        System.out.println("La temperatura maxima es: " + tempMax);
+        System.out.println("El promedio de temperatura es: " + (acum / arr.length));
 
     }
->>>>>>> c48ae2baaf068c75fe3e8501cf71961d9f50609d
+
+    public static void punto20() {
+        double[] numbers = new double[10];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese el numero 1:");
+        int n1 = scan.nextInt();
+        System.out.println("Ingrese el numero 2:");
+        int n2 = scan.nextInt();
+
+        scan.close();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (n1 > n2) {
+                numbers[i] = Math.random() * (n1 - n2) + n2;
+            } else {
+                numbers[i] = Math.random() * (n2 - n1) + n1;
+            }
+        }
+
+        System.out.println("Numeros ingresados: " + n1 + " " + n2);
+
+        for (double number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+
+    public static void punto21() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ingrese un dia de la semana por favor:");
+        String day = scan.next().toLowerCase();
+
+        switch (day) {
+            case "lunes":
+                System.out.println("Es dia laboral");
+                break;
+            case "martes":
+                System.out.println("Es dia laboral");
+                break;
+            case "miercoles":
+                System.out.println("Es dia laboral");
+                break;
+            case "jueves":
+                System.out.println("Es dia laboral");
+                break;
+            case "viernes":
+                System.out.println("Es dia laboral");
+                break;
+            case "sabado":
+                System.out.println("No es dia laboral");
+                break;
+            case "domingo":
+                System.out.println("No es dia laboral");
+                break;
+
+            default:
+                System.out.println("No es un dia valido");
+                break;
+        }
+    }
+
+    public static void punto22() {
+        Scanner scan = new Scanner(System.in);
+        boolean flag = true;
+        int number;
+        int cont = 0;
+
+        do {
+            System.out.println("Ingrese un numero positivo: ");
+            number = scan.nextInt();
+
+            if( number > 0) {
+                flag = false;
+            }
+
+        } while(flag);
+
+        while(number > 0) {
+            cont++;
+            number = number/10;
+        }
+
+        System.out.println("El numero tiene " + cont + " digitos");
+    }
+
 
 }
