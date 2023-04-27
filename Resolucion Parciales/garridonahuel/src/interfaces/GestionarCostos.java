fcb5a62194bb41d6bb95fb04d67d4cd1fdc233a7
Nahuel Garrido <garridonahuel8@gmail.com>
2023-04-26 22:21:11 -0300

    correciones

--- Resolucion Parciales/garridonahuel/src/interfaces/GestionarCostos.java
+++ Resolucion Parciales/garridonahuel/src/interfaces/GestionarCostos.java
@@ -2,7 +2,7 @@ package interfaces;
 
 /// Implemento interfaz por escalabilidad a futuro.
 public interface GestionarCostos {
-    public abstract double calcularTarifaEnvio();
-    public abstract double calcularCostoAdicional();
-    public abstract double calcularTotalPaquete();
+    double calcularTarifaEnvio();
+    double calcularCostoAdicional();
+    double calcularTotalPaquete();
 }
