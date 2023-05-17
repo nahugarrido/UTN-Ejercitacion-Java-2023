dbf05086ca587aba67d503057faf7adea2e7c7bd
Nahuel <68355186+nahugarrido@users.noreply.github.com>
2023-05-17 19:36:53 -0300

    menu en libreta de contactos

--- Resolucion guias/Libreta-direcciones/src/Main.java
+++ Resolucion guias/Libreta-direcciones/src/Main.java
@@ -1,20 +1,83 @@
 import modelos.Contacto;
 import modelos.Libreta;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Libreta libreta = new Libreta();
+        Scanner scan = new Scanner(System.in);
+        boolean continuar = true;
+        String opcion = "";
 
-        Contacto contacto1 = new Contacto(123123);
-        Contacto contacto2 = new Contacto(456465, "Libertad 2525");
-        Contacto contacto3 = new Contacto(666666, "Jara 852", "Juan Pablo");
+        do {
+            System.out.println("MENU LIBRETA");
+            System.out.println("Opción 1 - Cargar contactos");
+            System.out.println("Opción 2 - Mostrar contactos");
+            System.out.println("Opción 3 - Buscar contacto (Carlos)");
+            System.out.println("Opción 4 - Eliminar contacto (Carlos)");
+            System.out.println("Opción 5 - Buscar contacto (no existe)");
+            System.out.println("Opción 6 - Eliminar contacto (no existe)");
+            System.out.println("Opción 7 - Contar contactos");
+            System.out.println("Opción 8 - Salir");
 
-        libreta.cargarContacto("Brahian", contacto1);
-        libreta.cargarContacto("Carlos", contacto2);
-        libreta.cargarContacto("bernardo", contacto3);
-        libreta.cargarContacto("Alberto", contacto3);
+            opcion = scan.nextLine();
 
-        System.out.println(libreta.mostrarContactos());
-
+            switch (opcion) {
+                case "1":
+                    Contacto contacto1 = new Contacto(123123);
+                    Contacto contacto2 = new Contacto(456465, "Libertad 2525");
+                    Contacto contacto3 = new Contacto(666666, "Jara 852", "Juan Pablo");
+                    System.out.println(libreta.cargarContacto("Brahian", contacto1));
+                    System.out.println(libreta.cargarContacto("Carlos", contacto2));
+                    System.out.println(libreta.cargarContacto("Alberto", contacto3));
+                    break;
+                case "2":
+                    System.out.println("MOSTRAR CONTACTOS:");
+                    System.out.println(libreta.mostrarContactos());
+                    break;
+                case "3":
+                    System.out.println("Buscar contacto: CARLOS");
+                    try {
+                        System.out.println(libreta.buscarContacto("Carlos"));
+                    } catch (RuntimeException e) {
+                        System.out.println(e);
+                    }
+                    break;
+                case "4":
+                    System.out.println("Eliminar contacto: CARLOS");
+                    try {
+                        System.out.println(libreta.borrarContacto("Carlos"));
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
+                case "5":
+                    System.out.println("Buscar contacto: INEXISTENTE");
+                    try {
+                        System.out.println(libreta.buscarContacto("INEXISTENTE"));
+                    } catch (RuntimeException e) {
+                        System.out.println(e);
+                    }
+                    break;
+                case "6":
+                    System.out.println("Eliminar contacto: INEXISTENTE");
+                    try {
+                        System.out.println(libreta.borrarContacto("INEXISTENTE"));
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
+                case "7":
+                    System.out.println("Contador de contactos: " + libreta.contarCantidadContactos());
+                    break;
+                case "8":
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Debes selecionar una opción valida...");
+                    break;
+            }
+        } while (continuar);
     }
 }
--- Resolucion guias/Libreta-direcciones/src/modelos/Libreta.java
+++ Resolucion guias/Libreta-direcciones/src/modelos/Libreta.java
@@ -10,6 +10,11 @@ public class Libreta    {
     }
 
     public String cargarContacto(String nombre, Contacto nuevoContacto) {
+
+        if(listaContactos.containsKey(nombre)) {
+            throw new RuntimeException("El contacto ya existe, debes escoger otro nombre");
+        }
+
         listaContactos.put(nombre, nuevoContacto);
         return "Contacto agregado con exito";
     }
@@ -20,10 +25,42 @@ public class Libreta    {
         for(Map.Entry<String,Contacto> item : listaContactos.entrySet()) {
             String nombre = item.getKey();
             Contacto contacto = item.getValue();
-            texto += "\"Contacto{nombre: " + nombre + ", " + contacto.toString() + '}' + "\n";
+            texto += "1" +
+                    "Contacto{nombre: " + nombre + ", " + contacto.toString() + '}' + "\n";
         }
 
         return texto;
     }
+
+    public String buscarContacto(String nombre) {
+
+        if(!listaContactos.containsKey(nombre)) {
+            throw new RuntimeException("El contacto no existe");
+        }
+
+      Contacto contacto = listaContactos.get(nombre);
+
+      return "Contacto encontrado: " + contacto.toString();
+    }
+
+    public String borrarContacto(String nombre) throws Exception
+     {
+         if(!listaContactos.containsKey(nombre)) {
+             throw new Exception("El contacto no existe");
+         }
+
+         listaContactos.remove(nombre);
+
+         return "contacto removido con exito";
+     }
+
+     public int contarCantidadContactos() {
+        int contador = 0;
+        for(Map.Entry<String,Contacto> item: listaContactos.entrySet()) {
+            contador++;
+        }
+
+        return contador;
+     }
 
 }
